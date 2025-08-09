59061908408e624150571b06e2a88e8b503bee45
Mahadev Rajkumar Swami <swamimahadev2110@gmail.com>
2025-08-10 04:52:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
     // --- PARTICLES.JS BACKGROUND ---
     particlesJS("particles-js", {
         "particles": {
-            "number": { "value": 100, "density": { "enable": true, "value_area": 800 } },
+            "number": { "value": 80, "density": { "enable": true, "value_area": 800 } },
             "color": { "value": "#00f7ff" },
             "shape": { "type": "circle" },
-            "opacity": { "value": 0.5, "random": true },
+            "opacity": { "value": 0.4, "random": true },
             "size": { "value": 2, "random": true },
             "line_linked": { "enable": true, "distance": 150, "color": "#00f7ff", "opacity": 0.1, "width": 1 },
             "move": { "enable": true, "speed": 1, "direction": "none", "out_mode": "out" }
